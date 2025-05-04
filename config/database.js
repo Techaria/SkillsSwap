f784810bb08a3be14a2e43100536b43e5d983d3d
lewis <you@example.com>
2025-05-04 12:03:24 +0300

    WIP: local changes

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
-// config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('mywebsite', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(
+  'sql12776793', // your DB name
+  'sql12776793', // your username
+  'BRah1V7Eva', // your password
+  {
+    host: 'sql12.freesqldatabase.com', // your host
+    port: 3306, // default MySQL port
     dialect: 'mysql',
-});
+  }
+);
 
 sequelize.authenticate()
-    .then(() => {
-        console.log('Connected to MySQL using Sequelize');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MySQL:', err);
-    });
+  .then(() => console.log('Connected to MySQL (FreeSQLDatabase.com)'))
+  .catch((err) => console.error('Connection error:', err));
 
 module.exports = sequelize;
